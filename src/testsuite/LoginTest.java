9ddb7bf338ade42dd2f2fb45d63fb58498f10424
avinashp22 <avinashp22@gmail.com>
2023-11-20 22:49:20 +0000

    avinash-week11-com-orangehrmlive-sw-2

--- src/testsuite/LoginTest.java
+++ src/testsuite/LoginTest.java
@@ -2,8 +2,10 @@ package testsuite;
 
 import browserfactory.BaseTest;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 
 public class LoginTest extends BaseTest {
 
@@ -13,7 +15,15 @@ public class LoginTest extends BaseTest {
     public void setUp(){openBrowser(baseURL);}
 
     @Test
+    public void userShouldLoginSuccessfullyWithValidCredentials(){
+        driver.findElement(By.name("username")).sendKeys("Admin");
+        driver.findElement(By.name("password")).sendKeys("admin123");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        String expected = "Dashboard";
+        String actual = driver.findElement(By.xpath("//h6[text()='Dashboard']")).getText();
+        Assert.assertEquals("Correct Message not Displayed",expected,actual);
 
+    }
 
     @After
     public void tearDown(){closeBrowser();}
